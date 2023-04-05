d10085497fba8d3c6d103b0c8587138a373c274f
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-04-05 20:44:08 +0530

    working with list collection

---
+++ src/com/heysanjeet/collections/list_collection/ListConnection.java
@@ -0,0 +1,16 @@
+package com.heysanjeet.collections.list_collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListConnection {
+    public static void main(String[] args) {
+
+        List<String> nameList=new ArrayList<>();
+        nameList.add("Sanjeet");
+        nameList.add("Sunil");
+        nameList.add("Ramesh");
+
+        System.out.println(nameList);
+    }
+}
