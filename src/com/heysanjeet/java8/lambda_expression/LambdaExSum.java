76475fdd6f5d23f441970fe17700781c80e504b3
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-04-26 22:38:02 +0530

    working with java8

--- src/com/heysanjeet/java8/lambda_expression/LambdaExSum.java
+++
@@ -1,5 +0,0 @@
-package com.heysanjeet.java8.lambda_expression;
-
-public interface LambdaExSum {
-    public int getSum(int num1, int num2);
-}
--- src/com/heysanjeet/java8/lambda_expression/SumClient.java
+++
@@ -1,8 +0,0 @@
-package com.heysanjeet.java8.lambda_expression;
-
-public class SumClient {
-    public static void main(String[] args) {
-        LambdaExSum sum = (value1, value2) -> value1 + value2;
-        System.out.println(sum.getSum(16,15));//
-    }
-}
