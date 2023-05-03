a840326b271806a673736d724b0c5b8df75211ad
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-05-03 23:12:42 +0530

    examples of static method in interface.

---
+++ src/com/heysanjeet/java8/static_methods/InterfaceWithMainMethod.java
@@ -0,0 +1,7 @@
+package com.heysanjeet.java8.static_methods;
+
+public interface InterfaceWithMainMethod {
+    public static void main(String[] args) {
+        System.out.println("Hello in Interface");
+    }
+}
