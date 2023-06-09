74bc1e5b367dc29e3ed81c0ad9c6130eda84a1ae
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-06-09 21:34:28 +0530

    java 8 interview questions

---
+++ src/com/heysanjeet/java8/interview_questions/FindFirstNonRepeatE.java
@@ -0,0 +1,17 @@
+package com.heysanjeet.java8.interview_questions;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FindFirstNonRepeatE {
+    public static void main(String[] args) {
+        String str="iloveyou";
+        String firstNonRepeat = Arrays.stream(str.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new,Collectors.counting()))
+                .entrySet().stream().filter(mapItems -> mapItems.getValue() == 1)
+                .findFirst().get().getKey();
+        System.out.println(firstNonRepeat);
+    }
+}
