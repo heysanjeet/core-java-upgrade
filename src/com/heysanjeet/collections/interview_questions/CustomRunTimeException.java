0f8684328ed71b79d051e4dccf8babc0c154ab02
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-06-10 23:40:05 +0530

    interview questions[create custom exceptions]

---
+++ src/com/heysanjeet/collections/interview_questions/CustomRunTimeException.java
@@ -0,0 +1,7 @@
+package com.heysanjeet.collections.interview_questions;
+
+public class CustomRunTimeException extends RuntimeException {
+    public CustomRunTimeException(String message) {
+        super(message);
+    }
+}
