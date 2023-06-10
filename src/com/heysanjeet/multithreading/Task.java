5b6df14a069e6e3902a514f46f80e26297baeefb
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-06-10 23:46:08 +0530

    Thread and Multithreading.

---
+++ src/com/heysanjeet/multithreading/Task.java
@@ -0,0 +1,11 @@
+package com.heysanjeet.multithreading;
+
+public class Task extends Thread{
+    @Override
+    public void run() {
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+        }
+    }
+}
+
