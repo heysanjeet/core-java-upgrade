48cf2c19e226cdde8b6993552311c776bb13b951
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-06-10 23:39:36 +0530

    interview questions

---
+++ src/com/heysanjeet/collections/interview_questions/CustomException.java
@@ -0,0 +1,7 @@
+package com.heysanjeet.collections.interview_questions;
+
+public class CustomException extends Exception {
+    public CustomException(String message) {
+        super(message);
+    }
+}
