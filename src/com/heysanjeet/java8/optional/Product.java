a0df3edf3af2f9f432fbcaa26696e55949b06c8c
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-06-22 22:44:05 +0530

    grouping by examples

---
+++ src/com/heysanjeet/java8/optional/Product.java
@@ -0,0 +1,35 @@
+package com.heysanjeet.java8.optional;
+
+public class Product {
+    private String productName;
+    private Double productPrice;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(Double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public Product(String productName, Double productPrice) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productName='" + productName + '\'' +
+                ", productPrice=" + productPrice +
+                '}';
+    }
+}
