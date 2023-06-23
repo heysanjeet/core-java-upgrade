b81c4f64e4167355cafa37b1dce9a5461d68aa77
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-06-23 23:34:50 +0530

    sorted by examples

--- src/com/heysanjeet/collections/set_collection/SortedSetClient.java
+++ src/com/heysanjeet/collections/set_collection/SortedSetClient.java
@@ -1,7 +1,20 @@
 package com.heysanjeet.collections.set_collection;
 
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class SortedSetClient {
     public static void main(String[] args) {
+
+        SortedSet<Integer> sortedSet=new TreeSet<>();
+        sortedSet.add(34);
+        sortedSet.add(12);
+        sortedSet.add(6);
+        sortedSet.add(97);
+        sortedSet.add(1);
+
+        sortedSet.forEach(System.out::println);
+
 
     }
 }
