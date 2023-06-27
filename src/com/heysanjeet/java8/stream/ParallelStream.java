16d7705dd7326e5ac20d19cb90af388a5df4a401
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-06-27 22:31:26 +0530

    ParallelStream examples

--- src/com/heysanjeet/java8/stream/ParallelStream.java
+++ src/com/heysanjeet/java8/stream/ParallelStream.java
@@ -5,8 +5,11 @@ import java.util.stream.IntStream;
 public class ParallelStream {
     public static void main(String[] args) {
         long start=System.currentTimeMillis();
+
         IntStream.range(1, 1000000).parallel().forEach(x-> System.out.println(x));
+
         long end=System.currentTimeMillis();
+
         System.out.println("Parallel stream time taken "+(end-start));
     }
 }
