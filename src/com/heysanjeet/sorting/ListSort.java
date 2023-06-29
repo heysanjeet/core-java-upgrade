83fa77e6403fb3eb9f813ad54159199141957d60
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-06-29 23:35:53 +0530

    remove distict in a list

--- src/com/heysanjeet/sorting/ListSort.java
+++ src/com/heysanjeet/sorting/ListSort.java
@@ -1,13 +1,19 @@
 package com.heysanjeet.sorting;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class ListSort {
     public static void main(String[] args) {
-        int[] intArray = {1, 8, 6, 4, 5, 2, 9, 8, 10};
+        int[] intArray = {1, 8, 6,10, 4, 5,2, 2, 9, 8,4, 10};
+
+        //remove duplicate
+        List<Integer> distinctIntList = Arrays.stream(intArray).boxed().sorted().distinct().collect(Collectors.toList());
+        System.out.println(distinctIntList);
+
+        System.out.println("--------------------------------");
+
         //sort
         List<Integer> sortedList = Arrays.stream(intArray).boxed().sorted()
                 .collect(Collectors.toList());
