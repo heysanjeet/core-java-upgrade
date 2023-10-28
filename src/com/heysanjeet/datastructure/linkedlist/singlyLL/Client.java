57568e7751cf33a1b79c8bb7f64f2efc0ed12a00
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-10-28 23:48:00 +0530

    Datastructures[LinkedList]

---
+++ src/com/heysanjeet/datastructure/linkedlist/singlyLL/Client.java
@@ -0,0 +1,11 @@
+package com.heysanjeet.datastructure.linkedlist.singlyLL;
+
+public class Client {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(10);
+        linkedList.printList();
+        linkedList.getHead();
+        linkedList.getTail();
+        linkedList.length();
+    }
+}
---
+++ src/com/heysanjeet/datastructure/linkedlist/singlyLL/LinkedList.java
@@ -0,0 +1,34 @@
+package com.heysanjeet.datastructure.linkedlist.singlyLL;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        System.out.println("Head : " + head.data);
+    }
+
+    public void getTail() {
+        System.out.println("Tail : " + tail.data);
+    }
+
+    public void length() {
+        System.out.println(length);
+    }
+}
