94c36b0d12c983f15cc7cbc8af7eeb8ea08195cc
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-10-28 23:14:09 +0530

    Datastructures[LinkedList]

---
+++ src/com/heysanjeet/datastructure/linkedlist/Node.java
@@ -0,0 +1,10 @@
+package com.heysanjeet.datastructure.linkedlist;
+
+public class Node {
+    public Node next;
+    public int data;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
