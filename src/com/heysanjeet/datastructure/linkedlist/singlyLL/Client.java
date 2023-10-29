b81b0baff4e2bc481f71649ce908bc3da42d6d41
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-10-29 19:16:53 +0530

    Datastructures[LinkedList]

--- src/com/heysanjeet/datastructure/linkedlist/singlyLL/Client.java
+++ src/com/heysanjeet/datastructure/linkedlist/singlyLL/Client.java
@@ -3,11 +3,14 @@ package com.heysanjeet.datastructure.linkedlist.singlyLL;
 public class Client {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList(10);
-//        linkedList.append(20);
-//        linkedList.prepend(50);
-        System.out.println("Removed First "+linkedList.removeFirst().data);
-//        System.out.println("Removed value "+linkedList.remove().data);
+        linkedList.append(20);
+        linkedList.append(50);
         linkedList.printList();
+        System.out.println("--------------------------");
+        System.out.println(linkedList.get(3).data);//50
+        //System.out.println("Removed First "+linkedList.removeFirst().data);
+//        System.out.println("Removed value "+linkedList.remove().data);
+        //linkedList.printList();
 //        linkedList.getHead();
 //        linkedList.getTail();
 //        linkedList.length();
--- src/com/heysanjeet/datastructure/linkedlist/singlyLL/LinkedList.java
+++ src/com/heysanjeet/datastructure/linkedlist/singlyLL/LinkedList.java
@@ -73,6 +73,17 @@ public class LinkedList {
         return temp;
     }
 
+    public Node get(int index) {
+        if (index < 0 || index > length) {
+            return null;
+        }
+        Node temp = head;
+        for (int i = 1; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
     public void printList() {
         Node temp = head;
         while (temp != null) {
