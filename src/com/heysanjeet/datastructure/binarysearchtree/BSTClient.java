a3ea3cca3c039f98ca17c5fc81e8afdfb12609ae
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-11-02 09:56:28 +0530

    Datastructures[Binary Search Tree]

--- src/com/heysanjeet/datastructure/binarysearchtree/BSTClient.java
+++ src/com/heysanjeet/datastructure/binarysearchtree/BSTClient.java
@@ -1,5 +1,17 @@
 package com.heysanjeet.datastructure.binarysearchtree;
 
 public class BSTClient {
+    public static void main(String[] args) {
+        BinarySearchTree myBST = new BinarySearchTree();
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(52);
+        myBST.insert(82);
+
+        myBST.insert(27);
+
+    }
 
 }
