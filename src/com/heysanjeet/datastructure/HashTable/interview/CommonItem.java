e5818acc851b4ac414eaf8ea4aad8af1cf189946
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-11-05 04:41:43 +0530

    Datastructures[HashTable]

--- src/com/heysanjeet/datastructure/HashTable/interview/CommonItem.java
+++ src/com/heysanjeet/datastructure/HashTable/interview/CommonItem.java
@@ -11,7 +11,7 @@ public class CommonItem {
     public static boolean itemInCommonNaive(int array1[], int array2[]) {
         for (int i = 0; i < array1.length; i++) {
             for (int j = 0; j < array2.length; j++) {
-                if (array1[i] == array1[j]) {
+                if (array1[i] == array2[j]) {
                     return true;
                 }
             }
