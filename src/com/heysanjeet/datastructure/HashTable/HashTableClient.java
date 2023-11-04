bef5cf98a93f83f66907be8aa4ae1d191d239675
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-11-05 04:00:47 +0530

    Datastructures[HashTable]

--- src/com/heysanjeet/datastructure/HashTable/HashTableClient.java
+++ src/com/heysanjeet/datastructure/HashTable/HashTableClient.java
@@ -2,8 +2,11 @@ package com.heysanjeet.datastructure.HashTable;
 
 public class HashTableClient {
     public static void main(String[] args) {
-        HashTable myHashTable=new HashTable();
-
+        HashTable myHashTable = new HashTable();
+        myHashTable.set("Sanjeet", 10);
+        myHashTable.set("Sunil", 20);
+        myHashTable.set("Manish", 30);
+        myHashTable.set("Sanjeet", 40);
         myHashTable.printTable();
     }
 }
