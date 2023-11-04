870614470abe4dfd667ae6bf58b435087d93d112
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-11-05 03:58:46 +0530

    Datastructures[HashTable]

--- src/com/heysanjeet/datastructure/HashTable/HashTable.java
+++ src/com/heysanjeet/datastructure/HashTable/HashTable.java
@@ -8,6 +8,32 @@ public class HashTable {
         dataMap = new Node[size];
     }
 
+    public void set(String key, int value) {
+        int index = hash(key);
+        Node newNode = new Node(key, value);
+        if (dataMap[index] == null) {
+            dataMap[index] = newNode;
+        } else {
+            Node temp = dataMap[index];
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    private int hash(String key) {
+        int hash = 0;
+        char[] keyChars = key.toCharArray();
+        for (int i = 0; i < keyChars.length; i++) {
+            int asciiValue = keyChars[i];
+            //multiply with any prime no like 23.the no you will get is more random
+            //if % dataMap.length you will get the index value within the range.
+            hash = (hash + asciiValue * 23) % dataMap.length;
+        }
+        return hash;
+    }
+
     public void printTable() {
         for (int i = 0; i < dataMap.length; i++) {
             System.out.println("-------i------ " + i);
--- src/com/heysanjeet/datastructure/HashTable/HashTableClient.java
+++ src/com/heysanjeet/datastructure/HashTable/HashTableClient.java
@@ -3,6 +3,7 @@ package com.heysanjeet.datastructure.HashTable;
 public class HashTableClient {
     public static void main(String[] args) {
         HashTable myHashTable=new HashTable();
+
         myHashTable.printTable();
     }
 }
