f98938c1a6d94b31921474acea7bba1e86d9ea67
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-11-05 05:43:05 +0530

    Datastructures[HashTable]

--- src/com/heysanjeet/datastructure/graph/GraphClient.java
+++ src/com/heysanjeet/datastructure/graph/GraphClient.java
@@ -3,6 +3,7 @@ package com.heysanjeet.datastructure.graph;
 public class GraphClient {
     public static void main(String[] args) {
         Graph graph=new Graph();
+        graph.addVertex("A");
         graph.printGraph();
     }
 }
