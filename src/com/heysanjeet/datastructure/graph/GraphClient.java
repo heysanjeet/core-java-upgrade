ea82b03febe799c9587e842f050a8eccde497671
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-11-05 06:19:21 +0530

    Datastructures[Graph]

--- src/com/heysanjeet/datastructure/graph/GraphClient.java
+++ src/com/heysanjeet/datastructure/graph/GraphClient.java
@@ -5,8 +5,18 @@ public class GraphClient {
         Graph graph = new Graph();
         graph.addVertex("A");
         graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
         //add adge
-       graph.addEdge("A", "B");
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "C");
+        graph.addEdge("A", "D");
+        graph.addEdge("B", "D");
+        graph.addEdge("C", "D");
+        graph.printGraph();
+
+        System.out.println("------------------------------");
+        graph.removeVertex("D");
         graph.printGraph();
     }
 }
