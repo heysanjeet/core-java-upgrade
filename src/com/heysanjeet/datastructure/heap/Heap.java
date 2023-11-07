667418801460748c679cfb84ef7f1d489cf854c7
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-11-07 20:38:19 +0530

    Heap

---
+++ src/com/heysanjeet/datastructure/heap/Heap.java
@@ -0,0 +1,12 @@
+package com.heysanjeet.datastructure.heap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Heap {
+    private List<Integer> heap;
+
+    public Heap() {
+        this.heap = new ArrayList<>();
+    }
+}
