e8e94ad438e4a2df9cda244bb75cf1455bad96c2
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-11-16 22:17:48 +0530

    Heap

--- src/com/heysanjeet/datastructure/heap/Heap.java
+++ src/com/heysanjeet/datastructure/heap/Heap.java
@@ -10,4 +10,26 @@ public class Heap {
         this.heap = new ArrayList<>();
 
     }
+
+    private void swap(int index1, int index2) {
+        int temp = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, temp);
+    }
+
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    private int rightChild(int index) {
+        return 2 * index + 2;
+    }
+
+    public List<Integer> getHeap() {
+        return new ArrayList<>(heap);
+    }
 }
