00b285d2f8ac50023ed8134a4298dd0985abdbbe
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-11-21 00:10:12 +0530

    Heap

--- src/com/heysanjeet/datastructure/heap/Heap.java
+++ src/com/heysanjeet/datastructure/heap/Heap.java
@@ -15,10 +15,43 @@ public class Heap {
     public void insert(int value) {
         heap.add(value);
         int current = heap.size() - 1;
-        while (current > 0 && heap.get(current)>heap.get(parent(current))) {
+        while (current > 0 && heap.get(current) > heap.get(parent(current))) {
             swap(current, parent(current));
             current = parent(current);
 
+        }
+    }
+
+    public Integer remove() {
+        if (heap.size() == 0) {
+            return null;
+        }
+        if (heap.size() == 1) {
+            return heap.remove(0);
+        }
+        int maxValue = heap.get(0);
+        heap.set(0, heap.remove(heap.size() - 1));
+        sinkDown(0);
+        return maxValue;
+    }
+
+    private void sinkDown(int index) {
+        int maxIndex = index;
+        while (true) {
+            int leftIndex = leftChild(index);
+            int rightIndex = rightChild(index);
+            if (leftIndex < heap.size() && heap.get(leftIndex) > heap.get(maxIndex)) {
+                maxIndex = leftIndex;
+            }
+            if (rightIndex < heap.size() && heap.get(rightIndex) > heap.get(maxIndex)) {
+                maxIndex = rightIndex;
+            }
+            if (maxIndex != index) {
+                swap(index, maxIndex);
+                index = maxIndex;
+            } else {
+                return;
+            }
         }
     }
 
--- src/com/heysanjeet/datastructure/heap/HeapClient.java
+++ src/com/heysanjeet/datastructure/heap/HeapClient.java
@@ -4,12 +4,19 @@ public class HeapClient {
     public static void main(String[] args) {
 
         Heap heap = new Heap();
-        heap.insert(99);
-        heap.insert(61);
-        heap.insert(58);
-        heap.insert(72);
-        heap.insert(100);
+        heap.insert(95);
+        heap.insert(75);
+        heap.insert(80);
+        heap.insert(55);
+        heap.insert(60);
+        heap.insert(50);
+        heap.insert(65);
 
+
+        System.out.println(heap.getHeap());
+        heap.remove();
+        System.out.println(heap.getHeap());
+        heap.remove();
         System.out.println(heap.getHeap());
 
     }
