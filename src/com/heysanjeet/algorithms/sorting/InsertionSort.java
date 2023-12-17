366be8c2558d23ea59037b1bfa2b3da1de8e216e
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-12-18 01:01:43 +0530

    Insertion Sort.

---
+++ src/com/heysanjeet/algorithms/sorting/InsertionSort.java
@@ -0,0 +1,26 @@
+package com.heysanjeet.algorithms.sorting;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] input={4,5,1,2,6,3};
+        int[] result=insertionSort(input);
+        for(int i=0;i<result.length;i++){
+            System.out.print(result[i]+" ");
+        }
+
+    }
+
+    //always start from 2nd item
+    public static int[] insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+            while (j > -1 && temp < array[j]) {
+                array[j + 1] = array[j];
+                array[j] = temp;
+                j--;
+            }
+        }
+        return array;
+    }
+}
