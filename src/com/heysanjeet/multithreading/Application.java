d4cc1fa2fdddac5eeb9c3a4bd7e7812d0b16d9e1
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-12-21 22:17:10 +0530

    multithreading

--- src/com/heysanjeet/multithreading/Application.java
+++ src/com/heysanjeet/multithreading/Application.java
@@ -2,6 +2,6 @@ package com.heysanjeet.multithreading;
 
 public class Application {
     public static void main(String[] args) {
-
+        System.out.println("Java concurrency learning is started");
     }
 }
