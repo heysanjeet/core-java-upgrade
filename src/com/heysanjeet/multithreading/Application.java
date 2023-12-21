0264f32152ea5b475f8d3ff08ffdb84c00a345ec
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-12-21 21:56:42 +0530

    multithreading

--- src/com/heysanjeet/multithreading/Application.java
+++
@@ -1,7 +0,0 @@
-package com.heysanjeet.multithreading;
-
-public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello, there "+Thread.currentThread().getName());
-    }
-}
