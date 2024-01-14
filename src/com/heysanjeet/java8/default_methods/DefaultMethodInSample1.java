ad404b4142333212ea3998e71419be13c6f9e6f2
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2024-01-14 22:45:22 +0530

    default method

--- src/com/heysanjeet/java8/default_methods/DefaultMethodInSample1.java
+++ src/com/heysanjeet/java8/default_methods/DefaultMethodInSample1.java
@@ -1,4 +1,7 @@
 package com.heysanjeet.java8.default_methods;
 
 public interface DefaultMethodInSample1 {
+    public default void print(){
+        System.out.println("Print in sample1");
+    }
 }
---
+++ src/com/heysanjeet/java8/default_methods/DefaultMethodInSample2.java
@@ -0,0 +1,7 @@
+package com.heysanjeet.java8.default_methods;
+
+public interface DefaultMethodInSample2 {
+    public default void print(){
+        System.out.println("Print in sample2");
+    }
+}
