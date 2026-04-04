92fd28cac29f3f8ba12dca1b1988d9a994a00e25
sanjeetkumar <imsanjeetkr7@gmail.com>
2026-04-04 20:36:30 +0530

    refctored code

--- src/com/heysanjeet/sorting/ListSort.java
+++ src/com/heysanjeet/sorting/ListSort.java
@@ -8,12 +8,16 @@ public class ListSort {
     public static void main(String[] args) {
         int[] intArray = {1, 8, 6,10, 4, 5,2, 2, 9, 8,4, 10};
 
-        //remove duplicate
+        /**
+         * Get all the distinct integers.
+         */
         List<Integer> distinctIntList = Arrays.stream(intArray).boxed().sorted().distinct().collect(Collectors.toList());
         System.out.println(distinctIntList);
 
         System.out.println("--------------------------------");
-        //find even no
+        /**
+         * Get all the even numbers.
+         */
         distinctIntList.stream().filter(val->val%2==0).forEachOrdered(System.out::println);
 
         System.out.println("--------------------------------");
@@ -24,11 +28,11 @@ public class ListSort {
         //System.out.println(sortedList);
 
         //Reverse order
-        List<Integer> revSort = sortedList.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        List<Integer> revSort = sortedList.stream().sorted(Comparator.reverseOrder()).toList();
        // System.out.println(revSort);
 
         //Using parallel stream
-        List<Integer> integerList = sortedList.parallelStream().sorted().collect(Collectors.toList());
+        List<Integer> integerList = sortedList.parallelStream().sorted().toList();
         //integerList.forEach(System.out::println);
 
         integerList.stream().parallel().map(val->val+10).sorted()
